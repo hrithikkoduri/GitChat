ebc99f3a0eb05cfdf5d992a1f3f065840530b21e
Hrithik Koduri <hrithikkoduri@gmail.com>
2024-10-14 02:26:12 -0500

    Track large files with Git lfs

--- langchain-frontend-nextjs/pages/index.tsx
+++ langchain-frontend-nextjs/pages/index.tsx
@@ -34,7 +34,7 @@ export default function Home() {
 
     return (
         <div>
-            <h1>Langchain with fastAPI & Next.js</h1>
+            <h1>Langchain with fastAPI  & Next.js</h1>
             <form onSubmit={handleSubmit}>
                 <textarea
                     value = {question}
